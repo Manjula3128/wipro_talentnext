845a066afad54a21bfe8e5e9628accfc00a1c8c9
manjula3128 <manjulachakali2805@gmail.com>
2025-07-04 16:50:41 +0530

    java fun

---
+++ ManjuProject/src/ManjuProject/question2.java
@@ -0,0 +1,16 @@
+package ManjuProject;
+import java.util.*;
+public class question2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		String a=sc.nextLine();
+		System.out.println(a + " Welcome ");
+		
+
+
+
+	}
+
+}
