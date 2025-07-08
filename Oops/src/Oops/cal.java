37ceea06cc9f923fb04df51c8815d57bd9e55f56
manjula3128 <manjulachakali2805@gmail.com>
2025-07-08 15:09:29 +0530

    Oops

--- Oops/src/Oops/cal.java
+++ Oops/src/Oops/cal.java
@@ -1,5 +1,29 @@
 package Oops;
+import java.util.Scanner;
 
-public class cal {
+class Cal {
 
+    static int powerInt(int num1, int num2) {
+        return (int) Math.pow(num1, num2);
+    }
+
+    static double powerDouble(double num1, double num2) {
+        return Math.pow(num1, num2);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter integer base and exponent: ");
+        int intBase = sc.nextInt();
+        int intExp = sc.nextInt();
+        int intResult = powerInt(intBase, intExp);
+        System.out.println("Integer power result: " + intResult);
+
+        System.out.print("Enter double base and exponent: ");
+        double doubleBase = sc.nextDouble();
+        int doubleExp = sc.nextInt();
+        double doubleResult = powerDouble(doubleBase, doubleExp);
+        System.out.println("Double power result: " + doubleResult);
+    }
 }
