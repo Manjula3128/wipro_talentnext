89832cf153a0810e7afa879ddf8f7e30009b2d07
manjula3128 <manjulachakali2805@gmail.com>
2025-07-24 12:49:03 +0530

    Abstract

---
+++ Abstract/src/inheritancenewfeatures/QuestionInterfacesnewfeatures02.java
@@ -0,0 +1,16 @@
+package inheritancenewfeatures;
+import java.util.Scanner;
+		interface Sayable {
+		    void say();
+		}
+		public class QuestionInterfacesnewfeatures02 {
+		    public static void main(String[] args) {
+		        Scanner sc = new Scanner(System.in);
+		        System.out.println("Press any key and hit Enter to display message:");
+		        sc.nextLine();
+
+		        Sayable s = () -> System.out.println("This is say() method implemented using Lambda Expression.");
+		        s.say();
+		        sc.close();
+		    }
+		}
